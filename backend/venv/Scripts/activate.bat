f129e330198d3e4d660813c9186965584382d317
ajak-cp <67287017+ajak-cp@users.noreply.github.com>
2021-09-29 13:07:44 -0700

    Delete nested git

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\avaja\Desktop\CSC307\react-assignment1\backend\venv
+set VIRTUAL_ENV=C:\Users\avaja\Desktop\CPE307\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
